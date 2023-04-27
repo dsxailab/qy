eb504003cf2c0e61cf0556bce65f750bbed62acb
dsxailab <dsxailab@noreply.com>
2023-04-27 13:46:46 +0800

    publish

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -131,6 +131,12 @@ const RESOURCES = {
 
 
 
+
+
+
+
+
+
 "assets/AssetManifest.json": "80f0abc0f9a4817883efce43a88d46a6",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
@@ -150,9 +156,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "12154dab7f793a21bbf058bb0b19c467",
-"/": "12154dab7f793a21bbf058bb0b19c467",
-"main.dart.js": "c6e033a42b6c32e5f1b73ab0efbfbc22",
+"index.html": "ebee865a8f35eda9fab500a6bd1510c2",
+"/": "ebee865a8f35eda9fab500a6bd1510c2",
+"main.dart.js": "71008a30b3caee70b76f432eee8f4b0e",
 "manifest.json": "ac0e634aa81772bd0a4a529f5dd673fc",
 "version.json": "875beda4bbda2574bb3bd3f0db8dd52b"
 };
