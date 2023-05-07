90f29c0aaade656bec3556d0a80d1adc7e5d2ccb
dsxailab <dsxailab@noreply.com>
2023-05-07 19:27:35 +0800

    publish

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -54,9 +54,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "717b953a7b301920ef210a72958633ba",
 "icons/Icon-maskable-192.png": "f455524da0a95e61604f10443e95c995",
 "icons/Icon-maskable-512.png": "717b953a7b301920ef210a72958633ba",
-"index.html": "3e29c0fbc2569c086d5472ffb792f9f7",
-"/": "3e29c0fbc2569c086d5472ffb792f9f7",
-"main.dart.js": "27dc88e0ca381a020d197e63af713d35",
+"index.html": "d80a7ecc5769e49ea74bc68a27bd1374",
+"/": "d80a7ecc5769e49ea74bc68a27bd1374",
+"main.dart.js": "e4f9ddb59de5aec44daf9f37fda5c0de",
 "manifest.json": "ac0e634aa81772bd0a4a529f5dd673fc",
 "version.json": "875beda4bbda2574bb3bd3f0db8dd52b"
 };
